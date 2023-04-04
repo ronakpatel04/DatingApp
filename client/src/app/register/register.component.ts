52839d6c86d5f4e86b725e369e8ee916806282bb
RONAK MANGROLIYA <ronakpatel2378@gmail.com>
2023-04-04 18:18:13 +0530

    validation in register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -26,15 +26,18 @@ export class RegisterComponent implements OnInit{
     this.registerForm = new FormGroup({
         username : new FormControl('', Validators.required),
         password:new FormControl('', [Validators.required ,Validators.minLength(4), Validators.maxLength(8)]),
-        confirmPassword:new FormControl('',[Validators.required,this.matchValues('password')])
+        confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')])
 
+    })
+    this.registerForm.controls['password'].valueChanges.subscribe(()=>{
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity
     })
   }
 
   matchValues(matchTo:string):ValidatorFn
   {
     return  (control:AbstractControl) =>{
-      return control.value == control.parent?.get('matchTo')?.value ?null: {notMatching:true}
+      return control.value == control.parent?.get(matchTo)?.value ?null: {notMatching:true}
     }
   }
   
