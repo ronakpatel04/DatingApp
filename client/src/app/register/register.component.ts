f18b4f60e8ce68888cdd992b77617773ecc79a87
RONAK MANGROLIYA <ronakpatel2378@gmail.com>
2023-04-05 10:42:25 +0530

    extra field added in register form

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,5 +1,19 @@
-import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, NgForm, ValidatorFn, Validators } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  NgForm,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../services/account.service';
 
@@ -8,61 +22,69 @@ import { AccountService } from '../services/account.service';
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
 })
-export class RegisterComponent implements OnInit{
+export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   registerForm!: FormGroup;
 
-
-
   constructor(
     private accountService: AccountService,
-    private toastr: ToastrService
+    private toastr: ToastrService,
+    private fb: FormBuilder
   ) {}
   ngOnInit(): void {
-        this.initializeForm();
-  }
-  
-  initializeForm(){
-    this.registerForm = new FormGroup({
-        username : new FormControl('', Validators.required),
-        password:new FormControl('', [Validators.required ,Validators.minLength(4), Validators.maxLength(8)]),
-        confirmPassword: new FormControl('',[Validators.required,this.matchValues('password')])
-
-    })
-    this.registerForm.controls['password'].valueChanges.subscribe(()=>{
-      this.registerForm.controls['confirmPassword'].updateValueAndValidity
-    })
+    this.initializeForm();
   }
 
-  matchValues(matchTo:string):ValidatorFn
-  {
-    return  (control:AbstractControl) =>{
-      return control.value == control.parent?.get(matchTo)?.value ?null: {notMatching:true}
-    }
+  initializeForm() {
+    this.registerForm = this.fb.group({
+      knownAs: ['', Validators.required],
+
+      gender: ['male'],
+
+      dateOfBirth: ['', Validators.required],
+
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      username: ['', Validators.required],
+      password: [
+        '',
+        [Validators.required, Validators.minLength(4), Validators.maxLength(8)],
+      ],
+      confirmPassword: [
+        '',
+        [Validators.required, this.matchValues('password')],
+      ],
+    });
+    this.registerForm.controls['password'].valueChanges.subscribe(() => {
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity;
+    });
   }
-  
-  
-  
+
+  matchValues(matchTo: string): ValidatorFn {
+    return (control: AbstractControl) => {
+      return control.value == control.parent?.get(matchTo)?.value
+        ? null
+        : { notMatching: true };
+    };
+  }
+
   onRegister() {
-
-
-
-    console.log(this.registerForm.value)
+    console.log(this.registerForm.value);
 
     // this.accountService.register(this.registerForm.value).subscribe({
     //   next: (res) => {
     //     console.log(res);
     //     this.onCancel();
     //   },
-    //   error: (error) => { 
-      
+    //   error: (error) => {
+
     //     console.log(error);
     //     this.toastr.error(error);
-        //<------  if (error.error.errors.Username) {
-        //   this.toastr.error(error.error.errors.Username);
-        // } else {
-        //   this.toastr.error(error.error.errors.Password);
-        // } ---->
+    //<------  if (error.error.errors.Username) {
+    //   this.toastr.error(error.error.errors.Username);
+    // } else {
+    //   this.toastr.error(error.error.errors.Password);
+    // } ---->
     //   },
     // });
   }
